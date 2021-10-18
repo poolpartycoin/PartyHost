296c4d216b9430caff8a36023f08e93ccd53b150
JR White <white8785@gmail.com>
2021-10-18 13:56:10 -0400

    Setting up replit automation

--- keep_alive.py
+++ keep_alive.py
@@ -7,6 +7,10 @@ app = Flask('')
 def home():
     return "Hello. I am alive!"
 
+@app.route('/refresh')
+def refresh():
+    return "I have refreshed."
+
 def run():
   app.run(host='0.0.0.0',port=8080)
 
